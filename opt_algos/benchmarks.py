a0f8eca9423ba68e84b06667cc671f8af8ac9cf0
thomson <thomson.yen@mail.utoronto.ca>
2025-01-21 15:07:59 -0500

    Add data model benchmark

--- opt_algos/benchmarks.py
+++ opt_algos/benchmarks.py
@@ -1,17 +1,18 @@
-'''
+"""
 This file contains benchmarks. For each benchmark, we create a class with a function
 func, which can be fed into an optimizer (see optimizers.py for a set of requirements
 this function should meet
-'''
+"""
 
+import data_model as dm
 import numpy as np
 import pandas as pd
-
 import sklearn.ensemble as sk_e
 import sklearn.metrics as sk_m
 
-class SimpleMixtureBenchmark():
-    '''
+
+class SimpleMixtureBenchmark:
+    """
     This SimpleMixtureBenchmark involves a function z=f(x,y). The "true" function is a
     Branin function (https://www.sfu.ca/~ssurjano/branin.html), and we add normal noise
     with a variance of 120.
@@ -34,7 +35,7 @@ class SimpleMixtureBenchmark():
 
     Dataset I will contain the full range of X and Y and not be shuffled - it will act as
     a validation set.
-    '''
+    """
 
     def __init__(self):
         # Generate the data
@@ -51,10 +52,10 @@ class SimpleMixtureBenchmark():
         self.budget_space = [1, 30]
 
     def func(self, z, x):
-        '''
+        """
         This function takes a fidelity and a set of dataset weights, and returns the
         model's performance on the validation set I for a successive fidelities.
-        '''
+        """
 
         # Normalize the weight variables
         x = [i / sum(x) for i in x]
@@ -62,41 +63,58 @@ class SimpleMixtureBenchmark():
         # Convert this configuration to a string so we can look it up in the trained
         # model dictionary
         x_s = str(x)
-        
+
         # Check whether we already have a trained model - if so retrieve it, if not
         # create a new one
         if x_s in self.trained_models:
             m = self.trained_models[x_s]
         else:
-            m = sk_e.RandomForestRegressor(n_estimators = 0,
-                                           random_state = self.seed,
-                                           max_depth    = 6,
-                                           warm_start   = True)
+            m = sk_e.RandomForestRegressor(
+                n_estimators=0, random_state=self.seed, max_depth=6, warm_start=True
+            )
             self.trained_models[x_s] = m
 
         # If the model has been trained before, we should only be seeing it again with a
         # higher budget (if the model was just created it'll have been created with a budget
         # of 0, so this will be true)
         assert z >= m.n_estimators
-    
+
         # Get the data we're training on
         # ------------------------------
         this_data = []
-        for ds, this_x in zip(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], x):
-            this_data.append(self.data[ds].sample(int(len(self.data[ds])*this_x), random_state=self.seed))
+        for ds, this_x in zip(["A", "B", "C", "D", "E", "F", "G", "H"], x):
+            this_data.append(
+                self.data[ds].sample(
+                    int(len(self.data[ds]) * this_x), random_state=self.seed
+                )
+            )
         this_data = pd.concat(this_data)
 
         # Train and store intermediate results
         # ------------------------------------
         if m.n_estimators == z:
-            print(f'WARNING: evaluating func at a repeat value of z (x = {x}, z = {z})')
-            return [[z, sk_m.mean_squared_error(self.data['I']['Z'], m.predict(self.data['I'][['X', 'Y']]))]]
-        
+            print(f"WARNING: evaluating func at a repeat value of z (x = {x}, z = {z})")
+            return [
+                [
+                    z,
+                    sk_m.mean_squared_error(
+                        self.data["I"]["Z"], m.predict(self.data["I"][["X", "Y"]])
+                    ),
+                ]
+            ]
+
         out = []
         while m.n_estimators < z:
             m.n_estimators += 1
-            m.fit(X=this_data[['X', 'Y']], y=this_data['Z'])
-            out.append([m.n_estimators, sk_m.mean_squared_error(self.data['I']['Z'], m.predict(self.data['I'][['X', 'Y']]))])
+            m.fit(X=this_data[["X", "Y"]], y=this_data["Z"])
+            out.append(
+                [
+                    m.n_estimators,
+                    sk_m.mean_squared_error(
+                        self.data["I"]["Z"], m.predict(self.data["I"][["X", "Y"]])
+                    ),
+                ]
+            )
 
         # Return
         # ------
@@ -104,24 +122,24 @@ class SimpleMixtureBenchmark():
 
     @staticmethod
     def _branin(x, y):
-        '''
+        """
         Return the branin function value at x, y, which we use as our data generating
         pattern
-        '''
+        """
 
         b = 5.1 / (4 * np.pi**2)
         c = 5 / np.pi
         r = 6
         s = 10
         t = 1 / (8 * np.pi)
-        
-        return (y - b * x**2 + c * x - r)**2 + s * (1 - t) * np.cos(x) + s
+
+        return (y - b * x**2 + c * x - r) ** 2 + s * (1 - t) * np.cos(x) + s
 
     def generate_data(self):
-        '''
+        """
         Generate the 9 datasets as described above. This function returns a dict out
         with entries A through I constaining the datasets described above.
-        '''
+        """
 
         out = {}
 
@@ -132,17 +150,17 @@ class SimpleMixtureBenchmark():
         y = np.linspace(0, 15, 200)
         X, Y = [i.ravel() for i in np.meshgrid(x, y)]
         Z = self._branin(X, Y) + np.random.normal(0, 120, len(X))
-        df = pd.DataFrame(np.vstack([X, Y, Z]).T, columns=['X', 'Y', 'Z'])
+        df = pd.DataFrame(np.vstack([X, Y, Z]).T, columns=["X", "Y", "Z"])
 
-        out['A'] = df[(df.X <= 2.5) & (df.Y <= 7.5)]
-        out['B'] = df[(df.X > 2.5) & (df.Y <= 7.5)]
-        out['C'] = df[(df.X <= 2.5) & (df.Y > 7.5)]
-        out['D'] = df[(df.X > 2.5) & (df.Y > 7.5)]
+        out["A"] = df[(df.X <= 2.5) & (df.Y <= 7.5)]
+        out["B"] = df[(df.X > 2.5) & (df.Y <= 7.5)]
+        out["C"] = df[(df.X <= 2.5) & (df.Y > 7.5)]
+        out["D"] = df[(df.X > 2.5) & (df.Y > 7.5)]
 
-        for i, j in zip(['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H']):
+        for i, j in zip(["A", "B", "C", "D"], ["E", "F", "G", "H"]):
             out[j] = out[i].copy()
-            out[j].Z = np.random.choice(out[j].Z, len(out[j]), replace=False)*20
-        
+            out[j].Z = np.random.choice(out[j].Z, len(out[j]), replace=False) * 20
+
         # Dataset I
         # ---------
 
@@ -150,26 +168,27 @@ class SimpleMixtureBenchmark():
         y = np.linspace(0, 15, 100)
         X, Y = [i.ravel() for i in np.meshgrid(x, y)]
         Z = self._branin(X, Y) + np.random.normal(0, 120, len(X))
-        out['I'] = pd.DataFrame(np.vstack([X, Y, Z]).T, columns=['X', 'Y', 'Z'])
+        out["I"] = pd.DataFrame(np.vstack([X, Y, Z]).T, columns=["X", "Y", "Z"])
 
         # Store the data
         # --------------
         self.data = out
 
-class ConstantFunctionBenchmarkMixin():
-    '''
+
+class ConstantFunctionBenchmarkMixin:
+    """
     This mix-in class is used to create benchmarks based on a constant function. The
     benchmark class should inherit from this class and implement a _raw_func(z, x)
     method that returns a *single* value - the function evaluated at z and x.
 
     This class will then handle the memo-ization of past value, and the returning of a
     full trace, as is required by the optimizers
-    '''
+    """
 
     def __init__(self):
         # Create a dictionary that can store past evaluations
         self.past_evaluations = {}
-    
+
     def func(self, z, x):
         # Check whether we've already evaluated the function at this point
         x_s = str(x)
@@ -178,26 +197,27 @@ class ConstantFunctionBenchmarkMixin():
             z_start = 0
         else:
             z_start = self.past_evaluations[x_s][0]
-        
+
         if z_start == z:
-            print(f'WARNING: evaluating func at a repeat value of z (x = {x}, z = {z})')
+            print(f"WARNING: evaluating func at a repeat value of z (x = {x}, z = {z})")
             return [self.past_evaluations[x_s]]
-        
-        out = [[zz, self._raw_func(zz, x)] for zz in range(z_start+1, z+1)]
+
+        out = [[zz, self._raw_func(zz, x)] for zz in range(z_start + 1, z + 1)]
 
         self.past_evaluations[x_s] = out[-1]
 
         return out
 
+
 class BraninBenchmark(ConstantFunctionBenchmarkMixin):
-    '''
+    """
     Based on https://github.com/dragonfly/dragonfly/blob/master/examples/synthetic/branin/branin.py
     Modified to make fidelities integer values, and also to ensure that the fidelity is
     just one number
 
     In this example, func is simply a multi-fidelity version of a two-dimensional branin
     function https://www.sfu.ca/~ssurjano/branin.html
-    '''
+    """
 
     def __init__(self):
         # Set the search space and budget space
@@ -206,29 +226,31 @@ class BraninBenchmark(ConstantFunctionBenchmarkMixin):
 
         # Call the parent __init__
         super().__init__()
-    
+
     @staticmethod
     def _branin_with_params(x, a, b, c, r, s, t):
-        '''
+        """
         Computes the vanilla function
-        '''
+        """
 
         x1 = x[0]
         x2 = x[1]
-        neg_ret = float(a * (x2 - b*x1**2 + c*x1 - r)**2 + s*(1-t)*np.cos(x1) + s)
+        neg_ret = float(
+            a * (x2 - b * x1**2 + c * x1 - r) ** 2 + s * (1 - t) * np.cos(x1) + s
+        )
         return -neg_ret
 
     def _raw_func(self, z, x):
-        '''
+        """
         Branin with Multi-fidelity.
-        '''
+        """
 
         # The initial implementation is designed for z to be between 0 and 1
         z /= 100
 
-        b0 = 5.1/(4*np.pi**2)
-        c0 = 5/np.pi
-        t0 = 1/(8*np.pi)
+        b0 = 5.1 / (4 * np.pi**2)
+        c0 = 5 / np.pi
+        t0 = 1 / (8 * np.pi)
         delta_b = 0.01
         delta_c = 0.1
         delta_t = -0.005
@@ -241,13 +263,14 @@ class BraninBenchmark(ConstantFunctionBenchmarkMixin):
         t = t0 - (1.0 - z) * delta_t
         return -BraninBenchmark._branin_with_params(x, a, b, c, r, s, t)
 
+
 class LemurBenchmark(ConstantFunctionBenchmarkMixin):
-    '''
+    """
     This is a completely theoretical function we have designed for the purpose of testing
     BayesOpt algos.
 
     See readme file for details
-    '''
+    """
 
     def __init__(self):
         np.random.seed(123)
@@ -263,20 +286,20 @@ class LemurBenchmark(ConstantFunctionBenchmarkMixin):
         self.x = []
 
         # Dataset 1 contains lots of attribute 1 and 2
-        self.x.append([0.5, 0.5] + [0] * (self.K-2))
+        self.x.append([0.5, 0.5] + [0] * (self.K - 2))
 
         # Dataset 2 contains lots of attribute 3
-        self.x.append([0] * 2 + [0.5] + [0] * (self.K-3))
+        self.x.append([0] * 2 + [0.5] + [0] * (self.K - 3))
 
         # Dataset 3 contains the remaining attributes save for the last
-        self.x.append([0] * 3 + [0.5] * (self.K-4) + [0])
+        self.x.append([0] * 3 + [0.5] * (self.K - 4) + [0])
 
         # The remaining datasets contain only the last attribute
         for i in range(3, self.I):
-            this_x = [0] * (self.K-1)
+            this_x = [0] * (self.K - 1)
             this_x.append(1)
             self.x.append(this_x)
-        
+
         # Generate the baseline errors
         self.E = 1
         self.B = [np.random.uniform(0, 100) for k in range(self.K)]
@@ -289,7 +312,7 @@ class LemurBenchmark(ConstantFunctionBenchmarkMixin):
         self.budget_space = [1, 50]
 
         super().__init__()
-    
+
     def _raw_func(self, z, x):
         # Normalize the weight variables
         x = [i / sum(x) for i in x]
@@ -299,11 +322,45 @@ class LemurBenchmark(ConstantFunctionBenchmarkMixin):
 
         for k in range(self.K):
             this_out = self.B[k]
-            
+
             this_divisor = 0
             for i in range(self.I):
                 this_divisor += (x[i] * self.x[i][k] * z) ** self.beta[i]
-            
+
             out += this_out / this_divisor
-        
+
         return out
+
+
+class DataModelBenchmark(ConstantFunctionBenchmarkMixin):
+    """Create a data model benchmark that is compatible with 1D and 2D fidelity space"""
+
+    def __init__(self):
+        # Set the search space and budget space
+
+        # Logits for the 5 categories
+        self.search_space = [[-10, 10] for i in range(5)]
+        self.budget_space = [1, 196]
+
+        checkpoint_path = f"/mnt/home/tyen/data-recipes/opt_algos/data_models/20250112_111946_bvbk61r9"
+        self.model, self.norm_stats = dm.load_model_for_prediction(checkpoint_path)
+
+        super().__init__()
+
+    def _raw_func(self, z, x):
+        """Evaluate data model at step z and data mixture x"""
+
+        model_x = np.zeros(9)
+        proportions = np.exp(x) / np.sum(np.exp(x))
+        model_x[0:5] = proportions
+        model_x[8] = 100 * z  # Training steps
+
+        # Set other features to 1B features
+        model_x[5] = 20  # Model size in millions
+        model_x[6] = 256  # d_model dimension
+        model_x[7] = 8  # Number of attention heads
+
+        pred = dm.predict(
+            self.model, model_x.reshape(1, -1), norm_stats=self.norm_stats
+        )
+        return pred.squeeze()
---
+++ opt_algos/data_model.py
@@ -0,0 +1,344 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from pathlib import Path
+import json
+import plotly.graph_objects as go
+
+
+def load_checkpoint_run(run_dir):
+    """
+    Load checkpoint, configuration and feature mask from a specific run directory.
+
+    Args:
+        run_dir (str): Path to the run directory containing checkpoints, config and feature mask files
+                      (e.g., 'runs/20240315_123456_wandb_id')
+
+    Returns:
+        tuple: A tuple containing:
+            - checkpoint_data (dict): The loaded checkpoint containing model state and training info
+            - config_data (dict): Configuration parameters used for the run
+            - feature_mask_data (dict): Feature mask indicating which input features were used
+
+    Raises:
+        FileNotFoundError: If checkpoint, config or feature mask files don't exist
+        RuntimeError: If there are issues loading the checkpoint
+    """
+    run_path = Path(run_dir)
+
+    # Load latest checkpoint
+    checkpoint_path = run_path / "checkpoints" / "checkpoint_latest.pt"
+    checkpoint = torch.load(checkpoint_path)
+
+    # Load config
+    config_path = run_path / "config.json"
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    # Load feature mask
+    feature_mask_path = run_path / "feature_mask.json"
+    with open(feature_mask_path, "r") as f:
+        feature_mask = json.load(f)
+
+    return checkpoint, config, feature_mask
+
+
+class MLP(nn.Module):
+    """
+    Multi-layer perceptron model with configurable architecture and feature masking.
+
+    The model consists of:
+    - An input layer that applies feature masking
+    - Configurable number of hidden layers with ReLU activation and dropout
+    - A final output layer
+
+    Args:
+        input_size (int, optional): Number of input features. Defaults to 9.
+        hidden_size (int, optional): Number of neurons in hidden layers. Defaults to 64.
+        output_size (int, optional): Number of output dimensions. Defaults to 11.
+        num_layers (int, optional): Number of hidden layers. Defaults to 2.
+        dropout_rate (float, optional): Dropout probability between layers. Defaults to 0.2.
+        feature_mask (list[bool], optional): Boolean mask for selecting input features.
+            If None, all features are used. Defaults to None.
+
+    Attributes:
+        feature_mask (list[bool]): Active feature mask
+        layers (nn.Sequential): Sequential container of network layers
+    """
+
+    def __init__(
+        self,
+        input_size=9,
+        hidden_size=64,
+        output_size=11,
+        num_layers=2,
+        dropout_rate=0.2,
+        feature_mask=None,
+    ):
+        super(MLP, self).__init__()
+
+        assert hidden_size > 0, "Hidden size must be greater than 0"
+        assert num_layers >= 1, "Number of layers must be at least 1"
+        assert 0 <= dropout_rate < 1, "Dropout rate must be between 0 and 1"
+
+        self.feature_mask = (
+            feature_mask if feature_mask is not None else [True] * input_size
+        )
+        actual_input_size = sum(self.feature_mask)
+
+        layers = []
+        # Input layer
+        layers.append(nn.Linear(actual_input_size, hidden_size))
+        layers.append(nn.ReLU())
+        layers.append(nn.Dropout(dropout_rate))
+
+        # Hidden layers
+        for _ in range(num_layers - 1):
+            layers.append(nn.Linear(hidden_size, hidden_size))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout_rate))
+
+        # Output layer (no dropout after final layer)
+        layers.append(nn.Linear(hidden_size, output_size))
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        # Convert feature mask to boolean tensor on same device as input
+        mask = torch.tensor(self.feature_mask, device=x.device, dtype=torch.bool)
+        # Select features using the mask
+        x = x[:, mask]
+        return self.layers(x)
+
+
+def load_model_for_prediction(checkpoint_path):
+    """
+    Load a trained model and its normalization statistics from a checkpoint.
+
+    Args:
+        checkpoint_path (str): Path to directory containing model checkpoint and config files
+
+    Returns:
+        tuple: A tuple containing:
+            - model (MLP): Initialized and loaded model ready for prediction
+            - norm_stats (dict): Normalization statistics for input/output scaling
+                Contains 'X_mean', 'X_std', 'y_mean', 'y_std' for feature normalization
+
+    Raises:
+        FileNotFoundError: If checkpoint files don't exist
+        KeyError: If checkpoint is missing required data
+    """
+    # Load checkpoint, config and feature mask
+    checkpoint, config, feature_mask_data = load_checkpoint_run(checkpoint_path)
+
+    # Extract feature mask and convert to list of booleans
+    feature_mask = feature_mask_data.get("feature_mask", [True] * 9)
+    if isinstance(feature_mask, dict):
+        feature_mask = [
+            bool(feature_mask[str(i)]) if str(i) in feature_mask else True
+            for i in range(9)
+        ]
+
+    # Initialize model
+    model = MLP(
+        input_size=9,
+        hidden_size=config["hidden_size"],
+        output_size=11,
+        num_layers=config["num_layers"],
+        dropout_rate=config["dropout_rate"],
+        feature_mask=feature_mask,
+    )
+
+    model.load_state_dict(checkpoint["model_state_dict"])
+    norm_stats = checkpoint["normalization_stats"]
+
+    return model, norm_stats
+
+
+def predict(model, X, norm_stats=None):
+    """
+    Make predictions using the model with optional input/output normalization.
+
+    Args:
+        model (MLP): The trained model to use for prediction
+        X (numpy.ndarray): Input features of shape (batch_size, num_features)
+        norm_stats (dict, optional): Normalization statistics containing:
+            - X_mean: Mean of input features
+            - X_std: Standard deviation of input features
+            - y_mean: Mean of target values
+            - y_std: Standard deviation of target values
+            If None, no normalization is applied. Defaults to None.
+
+    Returns:
+        numpy.ndarray: Model predictions of shape (batch_size, num_outputs).
+            If norm_stats provided, predictions are denormalized to original scale.
+
+    Note:
+        Small epsilon (1e-8) is added to standard deviations to prevent division by zero
+    """
+    model.eval()
+    with torch.no_grad():
+        # Normalize input if needed
+        if norm_stats is not None:
+            # Add small epsilon to avoid division by zero
+            eps = 1e-8
+            X_std = np.where(norm_stats["X_std"] == 0, eps, norm_stats["X_std"])
+            X_normalized = (X - norm_stats["X_mean"]) / X_std
+            X_tensor = torch.FloatTensor(X_normalized)
+        else:
+            X_tensor = torch.FloatTensor(X)
+
+        # Get predictions
+        y_pred_normalized = model(X_tensor).numpy()
+
+        # Denormalize predictions if needed
+        if norm_stats is not None:
+            # Add small epsilon to avoid division by zero
+            y_std = np.where(norm_stats["y_std"] == 0, eps, norm_stats["y_std"])
+            y_pred = y_pred_normalized * y_std + norm_stats["y_mean"]
+        else:
+            y_pred = y_pred_normalized
+
+        return y_pred
+
+
+if __name__ == "__main__":
+    # NOTE: EXAMPLE USAGE, the above code is modular and can be used in other scripts.
+
+    def map_runs_by_split():
+        """
+        Scans through all run directories and creates a dictionary mapping
+        split types to their corresponding run directories
+        """
+        runs_dir = Path("runs")
+        splits_dict = {}
+
+        # Iterate through all directories in runs/
+        for run_dir in runs_dir.iterdir():
+            if not run_dir.is_dir() or run_dir.name == "wandb":
+                continue
+
+            # Try to load the config file
+            config_path = run_dir / "config.json"
+            if not config_path.exists():
+                continue
+
+            try:
+                with open(config_path, "r") as f:
+                    config = json.load(f)
+
+                # Get the split type from config
+                split_type = config.get("split_type")
+                if split_type:
+                    if split_type not in splits_dict:
+                        splits_dict[split_type] = []
+                    splits_dict[split_type].append(str(run_dir))
+
+            except json.JSONDecodeError:
+                print(f"Error reading config from {run_dir}")
+                continue
+
+        # Print the results in a formatted way
+        print("=== Runs by Split Type ===")
+        for split_type, runs in splits_dict.items():
+            print(f"\n{split_type}:")
+            for run in runs:
+                print(f"  - {run}")
+
+        return splits_dict
+
+    # Create the mapping
+    splits_dict = map_runs_by_split()
+
+    def analyze_token_proportion_smoothness(split_type, num_points=1000):
+        """
+        Analyzes how smoothly the model responds to changes in token proportions
+        by sweeping across one proportion while maintaining sum = 1
+        """
+        if split_type not in splits_dict:
+            raise ValueError(
+                f"Invalid split type. Must be one of: {list(splits_dict.keys())}"
+            )
+
+        model_path = splits_dict[split_type][0]  # Use first model from the split type
+        # checkpoint_path = Path(model_path) / "checkpoints" / "checkpoint_latest.pt"
+
+        # Load model and normalization stats using the modular function
+        model, norm_stats = load_model_for_prediction(model_path)
+        model.eval()
+
+        # Create sweep values for first proportion (0 to 1)
+        sweep_values = np.linspace(0, 1, num_points)
+
+        # Initialize results dictionary
+        results = {"sweep_values": sweep_values, "predictions": []}
+
+        # For each sweep value, distribute remaining probability equally
+        for p1 in sweep_values:
+            # Remaining probability to distribute among other 4 proportions
+            remaining = 1 - p1
+            if remaining < 0:
+                continue
+
+            # Distribute remaining equally among other proportions
+            other_props = remaining / 4
+
+            # Create input array [p1, p2, p3, p4, p5, *other_features]
+            x = np.zeros(9)  # Assuming 9 total features
+            x[0] = p1
+            x[1:5] = other_props  # Equal distribution for other proportions
+
+            # Set other features to some reasonable default
+            x[5] = 20  # Model size in millions
+            x[6] = 256  # d_model dimension
+            x[7] = 8  # Number of attention heads
+            x[8] = 15000  # Training steps
+
+            # Get prediction using the modular predict function
+            pred = predict(model, x.reshape(1, -1), norm_stats)
+            results["predictions"].append(pred.squeeze())
+
+        # Convert predictions to numpy array
+        results["predictions"] = np.array(results["predictions"])
+
+        # Create visualization
+        fig = go.Figure()
+
+        # Plot prediction for each output dimension
+        datasets = {
+            "train_cross_entropy": 0,
+            "commoncrawl": 1,
+            "c4": 2,
+            "wikipedia": 3,
+            "stackexchange": 4,
+            "github": 5,
+            "arxiv": 6,
+            "book": 7,
+            "hellaswag": 8,
+            "piqa": 9,
+            "arc_easy": 10,
+        }
+
+        for dataset, idx in datasets.items():
+            fig.add_trace(
+                go.Scatter(
+                    x=sweep_values,
+                    y=results["predictions"][:, idx],
+                    name=dataset,
+                    mode="lines",
+                )
+            )
+
+        fig.update_layout(
+            title="Model Predictions vs First Token Proportion (Denormalized)",
+            xaxis_title="First Token Proportion",
+            yaxis_title="Predicted Value",
+            height=600,
+            width=1000,
+        )
+
+        return fig, results
+
+    split_type = "single_step_15000_split"
+    fig, results = analyze_token_proportion_smoothness(split_type)
+    fig.show()
