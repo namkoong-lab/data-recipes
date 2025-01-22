4d45bc900662337dd57babff630f905e44331445
Tzu-Ching Yen <thomson.yen@mail.utoronto.ca>
2025-01-22 12:19:20 -0500

    Test datamodel benchmark in tests.py

--- opt_algos/tests.py
+++ opt_algos/tests.py
@@ -1,23 +1,22 @@
 # This files runs tests with all the optimizers on all the benchmarks
 
-import benchmarks
-import optimizers
 import pickle
 
-benchmarks = {'Branin'         : benchmarks.BraninBenchmark       ,
-              'SimpleMixture'  : benchmarks.SimpleMixtureBenchmark,
-              'Lemur'          : benchmarks.LemurBenchmark         }
+import benchmarks
+import optimizers
 
-optimizers = {'SMAC'         : optimizers.SMACOptimizer         ,
-              'BOCA'         : optimizers.BOCAOptimizer         ,
-              'RandomSearch' : optimizers.RandomSearchOptimizer ,
-              'GridSearch'   : optimizers.GridSearchOptimizer     }
+benchmarks = {"DataModel": benchmarks.DataModelBenchmark}
+
+optimizers = {
+    "SMAC": optimizers.SMACOptimizer,
+    "BOCA": optimizers.BOCAOptimizer,
+    "RandomSearch": optimizers.RandomSearchOptimizer,
+    "GridSearch": optimizers.GridSearchOptimizer,
+}
 
 # These are times are chosen (by trial an error) to get the number of budget evaluations
 # for SMAC roughly equal to 1000, which is what is used for the other techniques
-smac_times = {'Branin'        : 1.5,
-              'SimpleMixture' : 10,
-              'Lemur'         : 2   }
+smac_times = {"Branin": 1.5, "SimpleMixture": 10, "Lemur": 2, "DataModel": 10}
 
 # Run
 
@@ -27,14 +26,16 @@ for b in benchmarks:
     for o in optimizers:
         print(b, o)
 
-        if o == 'SMAC':
+        if o == "SMAC":
             max_time = smac_times[b]
         else:
             max_time = 1000
-        
+
         benchmark = benchmarks[b]()
 
-        out[(b,o)] = optimizers[o](benchmark.func, benchmark.search_space, benchmark.budget_space, max_time).minimize()
+        out[(b, o)] = optimizers[o](
+            benchmark.func, benchmark.search_space, benchmark.budget_space, max_time
+        ).minimize()
 
 # Save the results
-pickle.dump(out, open('tests.pickle', 'wb'))
+pickle.dump(out, open("tests.pickle", "wb"))
